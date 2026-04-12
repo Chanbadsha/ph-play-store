1c5cdcd20a7ca4072e6c55b060d3961b57b98183
Chan Badsha Bhuiyan <chanbadsha005@gmail.com>
2026-04-13 01:08:41 +0600

    Fix some bug

--- src/app/components/shared/NoAppInstalle/NoAppInstall.jsx
+++ src/app/components/shared/NoAppInstalle/NoAppInstall.jsx
@@ -1,12 +1,12 @@
 import Image from "next/image";
 import Link from "next/link";
-import errorImg from "@/assets/images/app-error.png"; // adjust path
+import notFoundImg from "@/assets/images/notfound.png"; // adjust path
 import PrimaryBtn from "@/app/utils/PrimaryBtn";
 
 const NoAppInstall = () => {
   return (
     <div className="flex mt-12 bg-white rounded-lg py-12 h-full flex-1 container mx-auto justify-center items-center flex-col text-center">
-      <Image src={errorImg} alt="Not Found Logo" height={300} width={300} />
+      <Image src={notFoundImg} alt="Not Found Logo" height={300} width={300} />
 
       <h1 className="text-xl md:text-2xl my-3 lg:text-4xl font-semibold">
         No Installed Apps Found
