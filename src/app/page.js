7e93ce6a8f276b1c2a6032b018ffb13f29162b1b
Chan Badsha Bhuiyan <chanbadsha005@gmail.com>
2026-04-13 00:59:11 +0600

    Installation app and delete app fucntionaly complete

--- src/app/page.js
+++ src/app/page.js
@@ -2,7 +2,11 @@ import React from "react";
 import Hero from "./components/homepage/Hero";
 import StatesSection from "./components/homepage/States";
 import TrendingApps from "./components/homepage/TrendingApps";
-
+export const metadata = {
+  title: "PH Play Store",
+  description:
+    "A modern app store where you can explore, install, and manage apps with ease.",
+};
 const HomePage = () => {
   return (
     <div>
